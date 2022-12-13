1f2f314757734af7fd6b17330cad149ee0bb08b2
jinxuguang <jinxuguang@mxbc.com>
2022-12-13 20:05:52 +0800

    Organize application with package

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -1,3 +1,4 @@
+# 包构造文件，创建程序实例
 import os
 import sys
 
@@ -16,7 +17,8 @@ app = Flask(__name__)
 # session 用来在请求间存储数据，它会把数据签名后存储到浏览器的 Cookie 中，所以我们需要设置签名所需的密钥：
 # 这个密钥的值在开发时可以随便设置。基于安全的考虑，在部署时应该设置为随机字符，且不应该明文写在代码里
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
-# 数据库文件一般放到项目根目录
+# 注意更新这里的路径，把 app.root_path 添加到 os.path.dirname() 中
+# 以便把文件定位到项目根目录
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),
                                                               os.getenv('DATABASE_FILE', 'data.db'))
 # 关闭对模型修改的监控
@@ -39,6 +41,7 @@ login_manager.login_view = 'login'
 # 创建用户加载回调函数，接受用户 ID 作为参数
 @login_manager.user_loader
 def load_user(user_id):
+    # 使用的模型类也在函数内进行导入
     from watchlist.models import User
     # 用 ID 作为 User 模型的主键查询对应的用户
     user = User.query.get(int(user_id))
@@ -47,10 +50,13 @@ def load_user(user_id):
 
 @app.context_processor
 def inject_user():
+    # 使用的模型类也在函数内进行导入
     from watchlist.models import User
     user = User.query.first()
     # 这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用
     return dict(user=user)
 
 
+# 在构造文件中，为了让视图函数、错误处理函数和命令函数注册到程序实例上，我们需要在这里导入这几个模块。
+# 因为这几个模块同时也要导入构造文件中的程序实例，为了避免循环依赖（A 导入 B，B 导入 A），我们把这一行导入语句放到构造文件的结尾。
 from watchlist import views, commands, errors
