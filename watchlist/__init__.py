e637ec9ec2ad33a08c0ee01d310f279bc2a247f9
jinxuguang <jinxuguang@mxbc.com>
2022-12-13 18:57:40 +0800

    Add base template and error template

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -6,41 +6,33 @@ from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
-
 WIN = sys.platform.startswith('win')
-if WIN:
+if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
-else:
+else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+# 数据库文件一般放到项目根目录
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),
                                                               os.getenv('DATABASE_FILE', 'data.db'))
+# 关闭对模型修改的监控
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
 # login_manager = LoginManager(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', name=name, movies=movies)
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    # 这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，因此可以直接在模板中使用
+    return dict(user=user)
 
 
-
+from watchlist import views, commands, errors
---
+++ watchlist/views.py
@@ -0,0 +1,13 @@
+from flask import render_template, request, url_for, redirect, flash
+from flask_login import login_user, login_required, logout_user, current_user
+
+from watchlist import app, db
+from watchlist.models import User, Movie
+
+
+@app.route('/')
+def index():
+    from watchlist.models import User, Movie
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
