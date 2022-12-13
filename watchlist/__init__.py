fb5e503b044ce111f11f93d9f350c4b5c1fac3a5
jinxuguang <jinxuguang@mxbc.com>
2022-12-13 17:09:34 +0800

    Add minimal home page

---
+++ watchlist/__init__.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+# SQLite URI compatible
+from flask import Flask, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),
+                                                              os.getenv('DATABASE_FILE', 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+
+@app.route('/')
+def hello():
+    return 'Hello'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {name}'
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
